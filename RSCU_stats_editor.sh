9ef61e6b0f42054d87f57b15b39de1fb0354edbb
Bryan Zavala <97570002+BRYZAV@users.noreply.github.com>
2023-04-17 14:45:31 -0400

    Update RSCU_stats_editor.sh

--- RSCU_stats_editor.sh
+++ RSCU_stats_editor.sh
@@ -4,7 +4,10 @@
 
 for file in *csv;
 do
+        #Remove first line of the file
         sed -i '1d' $file
+        #append the codon and its RSCU average value to a new file
         awk '{print $2","$3}' $file > Edited_$file
+        #Include their perspective headers in that file
         sed -i '1i Codon,Mean' Edited_$file
 done
